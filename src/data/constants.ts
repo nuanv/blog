3783d9758fe8d5d7503041a8bf6ce0cebbaa570b
Nihar Patel <73577147+nzyrk@users.noreply.github.com>
2024-12-27 11:30:29 +0530

    Update constants.ts

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,5 @@
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/niharPat",
+	github: "https://github.com/nzyrk",
 	linkedin: "https://www.linkedin.com/in/nihar-pat/",
 };
