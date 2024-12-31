eed9ab188179b5970e58c00e7a713d6bd62e8e81
Nihar Patel <73577147+nuanv@users.noreply.github.com>
2024-12-31 19:48:35 +0530

    Update Social Link

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,5 @@
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/nimzou",
+	github: "https://github.com/nuanv",
 	linkedin: "https://www.linkedin.com/in/nihar-pat/",
 };
